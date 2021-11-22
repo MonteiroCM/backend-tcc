4b4d71b371e5c72a46c1e56ccb89c4612cac1ad0
Monteiro <clayton.monteiro@gmail.com>
2021-11-22 20:27:13 -0300

    porta

--- src/app.js
+++ src/app.js
@@ -30,6 +30,6 @@ App.use((req, res, next) => {
   res.status(404).send({ error: '404 - Caminho não encontrado' })
 })
 
-const PORT = process.env.PORT || 8086
+const PORT = process.env.PORT || 5000
 
 App.listen(PORT, () => console.log(`Serviços disponíveis na porta ${PORT}`))
